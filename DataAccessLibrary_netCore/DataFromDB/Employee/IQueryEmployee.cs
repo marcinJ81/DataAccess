88b4e04c90bdc7275b84070f133f4ba47f02982f
Marcin Juranek <m.juranek@PGG.PL>
2021-03-10 09:19:57 +0100

    copy datafromDB and model

---
+++ DataAccessLibrary_netCore/DataFromDB/Employee/IQueryEmployee.cs
@@ -0,0 +1,14 @@
+﻿using DataAccessLibrary_netCore.DataAccess.Query;
+using DataAccessLibrary_netCore.Model;
+using System.Collections.Generic;
+using System.Data;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary_netCore.DataFromDB.Employee
+{
+    public interface IQueryEmployee
+    {
+        Task<List<ModelEmployee>> GetEmployees_async(dbType db_type);
+        List<ModelEmployee> GetEmployees_sync(dbType db_type);
+    }
+}
---
+++ DataAccessLibrary_netCore/Model/ModelEmployee.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessLibrary_netCore.Model
+{
+    public class ModelEmployee
+    {
+        public int employee_id { get; set; }
+        public string employee_name { get; set; }
+    }
+}
