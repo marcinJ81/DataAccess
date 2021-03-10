2f1146069be1ea0be52157db94aa5797b8aba1ee
Marcin Juranek <m.juranek@PGG.PL>
2021-03-10 11:34:46 +0100

    add mock IConfiguration from stackoverflow

---
+++ TestNunit/Test_DependencyContainer/SDependencyContainer.cs
@@ -0,0 +1,17 @@
+﻿using DataAccessLibrary_netCore.Dependency;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestNunit.Test_DependencyContainer
+{
+    public static class SDependencyContainer
+    {
+        private static ICreatorOfDBConnection creatorOfDBConnection { get; set; }
+        public static ICreatorOfDBConnection getCreatorOfDBConnection => (ICreatorOfDBConnection)creatorOfDBConnection;
+        static SDependencyContainer()
+        {
+            creatorOfDBConnection = new CreatorOfDBConnection();
+        }
+    }
+}
--- TestNunit/Test_Model/Test_MVCEmployeeModel.cs
+++ TestNunit/Test_Model/Test_MVCEmployeeModel.cs
@@ -1,18 +1,40 @@
-﻿using NUnit.Framework;
+﻿using DataAccessLibrary_netCore.DataAccess.Query;
+using DataAccessLibrary_netCore.DataFromDB.Employee;
+using DataAccessLibrary_netCore.Dependency;
+using DataAccessLibrary_netCore.Model;
+using Microsoft.Extensions.Configuration;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TestNunit.Test_DependencyContainer;
 
 namespace TestNunit.Test_Model
 {
    public class Test_MVCEmployeeModel
     {
 
-
+        private IConfiguration configuration;
+        private ICreatorOfDBConnection connectToMSSql;
+        private IQueryEmployee queryEmployee;
+        
         [SetUp]
         public void Setup()
         {
+            //mockhttps://stackoverflow.com/questions/64794219/how-to-mock-iconfiguration-getvalue
+            var inMemorySettings = new Dictionary<string, string> {
+                {"TopLevelKey", "TopLevelValue"},
+                {"ConnectionStrings:Production", "Data Source=ZIT-D0998\\SQL2014; Initial Catalog = salonfr2; Integrated Security = True; Connect Timeout = 30; Encrypt = False; TrustServerCertificate = False; ApplicationIntent = ReadWrite; MultiSubnetFailover = False"},
+                //...populate as needed for the test
+            };
+
+            this.configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection(inMemorySettings)
+                .Build();
+            var result = SDependencyContainer.getCreatorOfDBConnection;
+            this.connectToMSSql = result.CreateObject_MSsql(configuration, "Production");
             
         }
 
@@ -21,10 +43,14 @@ namespace TestNunit.Test_Model
         {
             // This workaround is necessary on Xamarin,
             // which doesn't support async unit test methods.
-            Task.Run(async () =>
-            {
-                // Actual test code here.
-            }).GetAwaiter().GetResult();
+            //Task.Run(async () =>
+            //{
+            //    // Actual test code here.
+            //}).GetAwaiter().GetResult();
+
+            queryEmployee = new QueryEmployee(this.connectToMSSql.GetISqlDataAccess);
+            List<ModelEmployee> result =  queryEmployee.GetEmployees_sync(dbType.mssql);
+            Assert.IsTrue(result.Any());
 
         }
     }
