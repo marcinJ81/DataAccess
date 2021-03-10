3295ca1340ca4eb938225410019db729db2322fb
Marcin Juranek <m.juranek@PGG.PL>
2021-03-10 08:56:31 +0100

    copy constring class

---
+++ DataAccessLibrary_netCore/ConString/ConnectStringAccess.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccessLibrary_netCore.ConString
+{
+    
+    public class ConnectStringAccess : IConnectStringAccess
+    {
+        private string ConnectionStringName { get; set; }
+        public string GetConString => (string)ConnectionStringName;
+       
+        public ConnectStringAccess(IConfiguration config, string connectionType)
+        {
+            string conTypes = SConnectionStringTypes.getdictionaryTypes.Where(x => x.Key == connectionType).FirstOrDefault().Value;
+            ConnectionStringName = config.GetConnectionString(conTypes);
+        }
+        public ConnectStringAccess(string tableNameSqlite)
+        {
+            string tablename = "";
+            string pathDbFile = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            if (!STableNameDictionary.getdTableName.Any(x => x.Key == tableNameSqlite))
+            {
+                tablename = STableNameDictionary.getdTableName.Where(x => x.Key == "Client").FirstOrDefault().Value;
+            }
+            else
+            {
+                tablename = STableNameDictionary.getdTableName.Where(x => x.Key == tableNameSqlite).FirstOrDefault().Value;
+            }
+            ConnectionStringName = new SqliteConnection(@"DataSource=" + pathDbFile + tablename).ConnectionString;
+        }
+    }
+}
---
+++ DataAccessLibrary_netCore/ConString/IConnectStringAccess.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace DataAccessLibrary_netCore.ConString
+{
+    public interface IConnectStringAccess
+    {
+        string GetConString { get; }
+    }
+}
