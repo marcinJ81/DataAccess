50b95caa06103dea7b7cd41bcdf2ae315f81fe54
Marcin Juranek <m.juranek@PGG.PL>
2021-03-16 09:24:57 +0100

    Create universal class for get any data model from DB

--- DataAccessLibrary_netCore/DataFromDB/T_Model/QueryT_UsingTableScripts.cs
+++ DataAccessLibrary_netCore/DataFromDB/T_Model/QueryT_UsingTableScripts.cs
@@ -1,11 +1,27 @@
-﻿using System;
+﻿using DataAccessLibrary_netCore.DataAccess.Query;
+using DataAccessLibrary_netCore.Model;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace DataAccessLibrary_netCore.DataFromDB.T_Model
 {
-    public class QueryT_UsingTableScripts
+    public class QueryT_WithSync_and_Async<T> : ICreateQueryFromDB<T>
     {
+        private readonly ISQLDataAccessQuery getDataFromDB;
+        public QueryT_WithSync_and_Async(ISQLDataAccessQuery sQLDataAccessQuery )
+        {
+            getDataFromDB = sQLDataAccessQuery;
+        }
+        public Task<List<T>> ASync_GetDataFromTable_Return_T(TableScripts tableScript, dbType db_type)
+        {
+            return getDataFromDB.loadData_async<T, dynamic>(tableScript.Script, tableScript.paramters, db_type);
+        }
 
+        public List<T> Sync_GetDataFromTable_Return_T(TableScripts tableScript, dbType db_type)
+        {
+            return getDataFromDB.loadData_sync<T, dynamic>(tableScript.Script, tableScript.paramters, db_type);
+        }
     }
 }
