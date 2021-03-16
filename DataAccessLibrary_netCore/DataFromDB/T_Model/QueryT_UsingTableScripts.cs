2080b17e08217d5897ab0dc640112bcd91e04f85
Marcin Juranek <m.juranek@PGG.PL>
2021-03-16 09:12:22 +0100

    add universal query class

---
+++ DataAccessLibrary_netCore/DataFromDB/T_Model/QueryT_UsingTableScripts.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessLibrary_netCore.DataFromDB.T_Model
+{
+    public class QueryT_UsingTableScripts
+    {
+
+    }
+}
